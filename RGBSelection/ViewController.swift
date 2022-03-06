05c4cfd97cbb55b74311a2ad645b1e4d41920784
Andrey Sokolov <hi@andreysokolov.com>
2022-03-06 15:22:28 +0300

    Add code ViewController

--- RGBSelection/ViewController.swift
+++ RGBSelection/ViewController.swift
@@ -8,25 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var rgbView: UIView!
+    
+    @IBOutlet var colorView: UIView!
     
     @IBOutlet var redNumberLabel: UILabel!
     @IBOutlet var greenNumberLabel: UILabel!
     @IBOutlet var blueNumberLabel: UILabel!
     
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    func colorViewChange() {
+        self.colorView.backgroundColor = UIColor (red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        rgbView.layer.cornerRadius = 20
+        colorView.layer.cornerRadius = 20
+        
+        redNumberLabel.text = String(redSlider.value)
+        greenNumberLabel.text = String(greenSlider.value)
+        blueNumberLabel.text = String(blueSlider.value)
+        
     }
     
-    @IBAction func redSliderValue(_ sender: Any) {
-        
-        
+    @IBAction func redSliderAction() {
+        redNumberLabel.text =  String(redSlider.value)
+        colorViewChange()
     }
     
-
-
+    @IBAction func greenSliderAction() {
+        greenNumberLabel.text = String(greenSlider.value)
+        colorViewChange()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueNumberLabel.text = String(blueSlider.value)
+        colorViewChange()
+    }
+    
+    
 }
 
